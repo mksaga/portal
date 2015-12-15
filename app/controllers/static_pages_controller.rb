7da06771f329ca25a7955254b8f1c11205e86f81
Mohamed <mohamed.aly06@gmail.com>
2015-12-15 22:50:40 +0000

    Initialize repository!

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+  
+  def loggedout
+  end
+  
+  def contact
+  end
+  
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "IST Portal"
+  end
+  
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get loggedout" do
+    get :loggedout
+    assert_response :success
+    assert_select "title", "Logged Out | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
+end
